7b82ffaf5d781e624ad21db3489569d1d127d241
Ronyssouza <128438560+Ronyssouza@users.noreply.github.com>
2023-03-29 00:27:15 -0300

    classe produto

--- src/main/java/entidade.java/Produto.java
+++ src/main/java/entidade.java/Produto.java
@@ -1,5 +1,24 @@
-package entidade;
 
 public class Produto {
+	public String nome;
+	public double preco;
+	public int quantidade;
+
+	public double totalValorEstoque() {
+		return preco * quantidade;
+	}
+
+	public void addProduto(int quantidade) {
+		this.quantidade += quantidade;
+	}
+
+	public void removeProduto(int quantidade) {
+		this.quantidade -= quantidade;
+	}
+
+	public String toString() {
+		return nome + ", $ " + String.format("%.2f", preco) + ", " + quantidade+ " Unidade, Total: $ "
+				+ String.format("%.2f", totalValorEstoque());
+	}
 
 }
