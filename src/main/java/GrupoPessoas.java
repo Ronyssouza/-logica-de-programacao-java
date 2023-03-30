b548e6c4f507b9f09e23fbe58e27738bf2066bf1
Ronyssouza <128438560+Ronyssouza@users.noreply.github.com>
2023-03-29 23:50:59 -0300

    resposta com class da questao 3

---
+++ src/main/java/GrupoPessoas.java
@@ -0,0 +1,18 @@
+
+public class GrupoPessoas {
+	private String nome;
+	private int idade;
+
+	public GrupoPessoas(String nome, int idade) {
+		this.nome = nome;
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+}
