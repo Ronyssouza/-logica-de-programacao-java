aefbe1e3c344fee00ab2f32ba7f7b6e387e19339
Ronyssouza <128438560+Ronyssouza@users.noreply.github.com>
2023-03-30 21:22:11 -0300

    acho que agora ta certo kkkk

--- src/main/java/GrupoPessoa.java
+++ src/main/java/GrupoPessoa.java
@@ -1,4 +1,32 @@
-
 public class GrupoPessoa {
+	public static void main(String[] args) {
+		Pessoas[] pessoas = { new Pessoas("Maria", 25), new Pessoas("João", 31), new Pessoas("Ana", 19),
+				new Pessoas("Pedro", 42), new Pessoas("Juliana", 38) };
 
+		int somaIdades = 0;
+		int maiorIdade = pessoas[0].getIdade();
+		int menorIdade = pessoas[0].getIdade();
+		String nomeMaisVelho = pessoas[0].getNome();
+		String nomeMaisNovo = pessoas[0].getNome();
+
+		for (Pessoas pessoa : pessoas) {
+			somaIdades += pessoa.getIdade();
+
+			if (pessoa.getIdade() > maiorIdade) {
+				maiorIdade = pessoa.getIdade();
+				nomeMaisVelho = pessoa.getNome();
+			}
+
+			if (pessoa.getIdade() < menorIdade) {
+				menorIdade = pessoa.getIdade();
+				nomeMaisNovo = pessoa.getNome();
+			}
+		}
+
+		double mediaIdade = (double) somaIdades / pessoas.length;
+
+		System.out.println("Media de idade: " + mediaIdade);
+		System.out.println("Pessoa mais velha: " + nomeMaisVelho);
+		System.out.println("Pessoa mais nova: " + nomeMaisNovo);
+	}
 }
